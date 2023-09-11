cb1e49c976590eb4f755b7b50caafa4061998b0e
Jhonata <jhonatamg1997@gmail.com>
2023-09-11 11:29:24 -0300

    230911

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,15 +1,33 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
-typedef struct {
-    double value;
-    int error;
-} result_t;
+typedef struct{
+    int v;
+    int tam [100];
+} pessoa_t;
 
-typedef enum { SUM, SUB } operation_t;
+/** Cria um novo vetor */
+int criar (int v, int tam);
 
-result_t sub(double a, double b);
-result_t sum(double a, double b);
-result_t compute(double a, double b, operation_t op);
+/** insere um linearmente */
+int inserir (int v, int tam);
+
+/** insere de forma ordenada */
+int inserir_o (int v, int tam);
+
+/** busca linearmente no v */
+int busca_lin (int v, int tam);
+
+/** busca binaria no v */
+int busca_bin (int v, int tam);
+
+/** */
+int remover ();
+
+/** */
+int remover_o ();
+
+/** */
+int limpa_men ();
 
 #endif
